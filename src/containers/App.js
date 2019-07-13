f5d8df002a7ce71dd20a43fb0aa6a20abf976040
web2ls <web2ls@yandex.ru>
2019-07-13 22:42:49 +0300

    added Error Boundry

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 export default class App  extends Component {
@@ -33,7 +34,9 @@ export default class App  extends Component {
                 <h1>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <Scroll>
-                    <CardList robots={filteredRobots} />
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots} />
+                    </ErrorBoundry>
                 </Scroll>
             </div>
         )
