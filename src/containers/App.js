e9b4aec46bb611eb7aec97460dbba9b9a8c32ac1
web2ls <web2ls@yandex.ru>
2019-07-18 00:57:54 +0300

    added async data via meddleware

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,42 +6,39 @@ import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-import {setSearchField} from '../actions';
+import {setSearchField, requestRobots} from '../actions';
 
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error,
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: (event) => dispatch(requestRobots())
     }
 }
 
 class App  extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-        }
-    }
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
-        .then(users => this.setState({robots: users}))
-        .catch(error => console.error(error));
+        this.props.onRequestRobots();
     }
 
     render() {
-        const {searchField, onSearchChange} = this.props;
-        const {robots} = this.state;
+        const {searchField, robots, isPending, onSearchChange} = this.props;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase())
         });
 
-        return (
+        return isPending ?
+            <h1>Loading</h1> :
+        (
             <div className='tc'>
                 <h1>RoboFriends</h1>
                 <SearchBox searchChange={onSearchChange} />
--- src/reducers.js
+++ src/reducers.js
@@ -1,13 +1,40 @@
-import {CHANGE_SEARCH_FIELD} from './constants';
+import {
+    CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAILED
+} from './constants';
 
-const initialState = {
+const initialStateSearch = {
     searchField: ' '
-}
+};
 
-export const searchRobots = (state = initialState, action = {}) => {
+const initialStateRobots = {
+    isPending: false,
+    robots: [],
+    error: ''
+};
+
+export const searchRobots = (state = initialStateSearch, action = {}) => {
     switch (action.type) {
         case CHANGE_SEARCH_FIELD:
-            return Object.assign({}, state, {searchField: action.payload});
+            return Object.assign({}, state, { searchField: action.payload });
+
+        default:
+            return state;
+    }
+}
+
+export const requestRobots = (state = initialStateRobots, action = {}) => {
+    switch (action.type) {
+        case REQUEST_ROBOTS_PENDING:
+            return Object.assign({}, state, {isPending: true});
+
+        case REQUEST_ROBOTS_SUCCESS:
+            return Object.assign({}, state, { robots: action.payload, isPending: false });
+
+        case REQUEST_ROBOTS_FAILED:
+            return Object.assign({}, state, {error: action.payload, isPending: false});
 
         default:
             return state;
